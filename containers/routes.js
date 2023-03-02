438593c7be85185314a74c38d0c1e1d62d400ce2
David Nelson <larva.code15@gmail.com>
2023-03-03 00:58:05 +0100

    Working on giftcard

--- containers/routes.js
+++ containers/routes.js
@@ -11,6 +11,7 @@ const error = require('../middleware/error');
 var whitelist = [
     'http://localhost:5173',
     'http://localhost:5174',
+    'http://localhost:5175',
     'https://spacebank-app.vercel.app',
     'https://spacebank.com'
 ];
--- routes/transaction.js
+++ routes/transaction.js
@@ -17,8 +17,6 @@ router.get("/:id", [Auth], async (req, res) => {
 router.post("/send-money/p2p", [Auth], async(req, res) => {
     const { username, amount, description, pin } = req.body;
 
-    console.log("PIN: ", pin);
-
     const sender = await User.findOne({ _id: req.user._id });
     if (String(sender.transactionPassword)!==String(pin)) return failedResponse(res, 400, "Incorrect transaction pin");
 
@@ -92,7 +90,7 @@ router.post("/data/buy-data", [Auth], async (req, res)  => {{
     const user = await User.findById(req.user._id);
     const userBalance = await Balance.findOne({ user: user });
 
-    console.log("BODY: ", req.body);
+    if (amount>parseInt(userBalance.balance)-1) return failedResponse(res, 400, 'Insuficient balance');
 
     var data = JSON.stringify({
         "phone": String(phone),
@@ -131,7 +129,61 @@ router.post("/data/buy-data", [Auth], async (req, res)  => {{
         })
         .catch(function (error) {
             console.log(error);
+            return failedResponse(res, 400, "An error occured");
         });
 }})
+
+router.post("/bill/pay-bill", [Auth], async (req, res) => {
+    const { service_id, customer_id, variation_code, amount, networkIcon } = req.body;
+    const user = await User.findById(req.user._id);
+    const userBalance = await Balance.findOne({ user: user });
+
+    if (amount>parseInt(userBalance.balance)-1) return failedResponse(res, 400, 'Insuficient balance');
+
+    var data = JSON.stringify({
+        "service_id": String(service_id),
+        "customer_id": String(customer_id),
+        "variation": String(variation_code),
+        "amount": String(amount)
+    });
+      
+    var config = {
+        method: 'POST',
+        url: 'https://bingpay.ng/api/v1/purchase-bill',
+        headers: { 
+            'Content-Type': 'application/json', 
+            'Authorization': 'Bearer 1dd501141dffd9d68f254b241f05871b8f10754c90f4832ad9'
+        },
+        data : data
+    };
+
+    axios(config)
+        .then(async function (response) {
+            const transaction = new Transaction({
+                user: user,
+                amount: parseInt(amount),
+                description: `You paid a bill of ${amount}`,
+                type: "pay-bill",
+                status: "confirmed",
+                icon: networkIcon,
+            })
+            userBalance.balance = parseInt(userBalance.balance)-parseInt(amount);
+            await transaction.save();
+            userBalance.save();
+            if (response.data.error){
+                console.log("ERROR 2: ", response.data);
+                return failedResponse(res, 400, response.data.message);
+            }
+            return successResponse(res, 200, response.data, `You recharged ₦${amount} to ${phone}`);
+        })
+        .catch(function (error) {
+            console.log(error);
+            return failedResponse(res, 400, "An error occured");
+        });
+})
+
+router.get("/giftcard/all", [Auth], async (req, res) => {
+    
+})
 
 module.exports = router;
